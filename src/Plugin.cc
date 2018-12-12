0aaee70cfca658b585100cac51aaece4fe74d9ec
Foster McLane <fkmclane@gmail.com>
2018-12-12 08:54:20 -0500

    update version number in source

--- src/Plugin.cc
+++ src/Plugin.cc
@@ -13,5 +13,6 @@ plugin::Configuration Plugin::Configure() {
 	config.description = "TCP log writer";
 	config.version.major = 0;
 	config.version.minor = 1;
+	config.version.patch = 1;
 	return config;
 }
